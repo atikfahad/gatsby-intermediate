525eba554f564a66db3645d9ae848a7910482021
Atik Fahad <fahadatik@gmail.com>
2020-04-03 04:11:35 +0600

    onCreateNode, createResolvers, createSchemaCustomization hooks created

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -2,6 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const withDefaults = require('./utils/default-options');
+
+// onPreBootStrap -> Before starting App
 exports.onPreBootstrap = ({ store }, options) => {
   const { program } = store.getState();
   // TODO get options with defaults
@@ -16,6 +18,7 @@ exports.onPreBootstrap = ({ store }, options) => {
   }
 };
 
+// createSchemaCustomization -> Create Schema in GraphQL
 exports.createSchemaCustomization = ({ actions }) => {
   // TODO it will create these nodes under allDocsPage >> nodes >> id, title, path, updated, body
   actions.createTypes(`
@@ -27,4 +30,53 @@ exports.createSchemaCustomization = ({ actions }) => {
         body: String!
     }
     `);
+};
+// onCreateNode -> in the time of creating Node
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options);
+  const parent = getNode(node.parent);
+
+  // Only work in MDX files that were loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-docs'
+  ) {
+    return;
+  }
+
+  // Treat `index.mdx` link `index.html` (i.e `docs/` vs. `docs/index/`)
+  const pageName = parent.name !== 'index' ? parent.name : '';
+
+  actions.createNode({
+    id: createNodeId(`DocsPage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    },
+  });
+};
+
+exports.createResolvers = ({ createResolvers }) => {
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+          // Load the resolver for the `Mdx` type `body` field.
+          const type = info.schema.getType('Mdx');
+          const mdxFields = type.getFields();
+          const resolver = mdxFields.body.resolve;
+          const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+
+          return resolver(mdxNode, args, context, {
+            fieldName: 'body',
+          });
+        },
+      },
+    },
+  });
 };
