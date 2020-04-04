2ecbe3735a403fd05fb4ef99bf00690448a99858
Atik Fahad <fahadatik@gmail.com>
2020-04-05 03:37:47 +0600

    two page creating in lookup site and configure gatsby-node to accept search urls parameter

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Index = () => (
+  <>
+    <h1>Search for Rick & Morty Character</h1>
+    <p>Trying to remember whic</p>
+    {/* TODO Add Search form */}
+    {/* TODO Add Result */}
+  </>
+);
+
+export default Index;
